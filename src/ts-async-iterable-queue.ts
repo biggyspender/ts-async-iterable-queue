f8554220b73075fad047e97a1895b952e9ada231
Chris Sperry <christophermartinsperry@gmail.com>
2019-04-16 22:10:12 +0100

    fix: reinstate node 8 support

--- src/ts-async-iterable-queue.ts
+++ src/ts-async-iterable-queue.ts
@@ -1,1 +1,6 @@
+/* istanbul ignore if */
+if (!Symbol.asyncIterator) {
+  const aiKey = 'asyncIterator'
+  ;(Symbol as any)[aiKey] = Symbol.for('Symbol.asyncIterator')
+}
 export { createAsyncQueue } from './createAsyncQueue'
